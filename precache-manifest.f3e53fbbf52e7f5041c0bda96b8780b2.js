1f26662665ba85d2844d9ce57769441ddeafaa84
tomer2077 <tfaibish@gmail.com>
2019-12-04 14:54:11 +0200

    Initial dist subtree commit

---
+++ precache-manifest.f3e53fbbf52e7f5041c0bda96b8780b2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b71a5b9f7915db2e675ab8ae6e51ff09",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e97070d393ad87044474",
+    "url": "/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "891e486a716533ac322c",
+    "url": "/static/css/main.ea013cf0.chunk.css"
+  },
+  {
+    "revision": "e97070d393ad87044474",
+    "url": "/static/js/2.814c5b91.chunk.js"
+  },
+  {
+    "revision": "891e486a716533ac322c",
+    "url": "/static/js/main.cb0071e4.chunk.js"
+  },
+  {
+    "revision": "9bf820bd41e6d656f8db",
+    "url": "/static/js/runtime-main.f59df13c.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
